8f11ccfc33aefe422dd2ae6aa90ddb432a5db6d5
obanleowo tofunmi <obanleowojoseph@gmail.com>
2022-11-07 13:54:35 +0100

    first commit

---
+++ js/custom.js
@@ -0,0 +1,23 @@
+$(function () {
+  // animate on scroll
+  new WOW().init();
+});
+$(function () {
+  $("#team-members").owlCarousel({
+    items: 3,
+    autoplay: true,
+    smartSpeed: 700,
+    loop: true,
+    autoplayHoverPause: true,
+  });
+});
+
+$(function () {
+  $("#customer-testimonials").owlCarousel({
+    items: 1,
+    autoplay: true,
+    smartSpeed: 700,
+    loop: true,
+    autoplayHoverPause: true,
+  });
+});
